7003ff345fce42bec7a7a695e63a5a7dbf772544
Estebitan88 <andygch@hotmail.com>
2023-09-24 13:09:25 -0500

    creacion de clase cucadrado y test clase cuadrado y sus funciones

---
+++ Clases/src/com/krakedev/TestCuadrado.java
@@ -0,0 +1,46 @@
+package com.krakedev;
+
+public class TestCuadrado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Cuadrado cuadrado1 = new Cuadrado();
+		Cuadrado cuadrado2 = new Cuadrado();
+		Cuadrado cuadrado3 = new Cuadrado();
+
+		double areaCuadrado1;
+		double areaCuadrado2;
+		double areaCuadrado3;
+
+		double perimetroCuadrado1;
+		double perimetroCuadrado2;
+		double perimetroCuadrado3;
+
+		cuadrado1.lado = 4;
+		cuadrado2.lado = 6;
+		cuadrado3.lado = 8;
+
+		areaCuadrado1 = cuadrado1.CalcularArea();
+		areaCuadrado2 = cuadrado2.CalcularArea();
+		areaCuadrado3 = cuadrado3.CalcularArea();
+
+		perimetroCuadrado1 = cuadrado1.calcularPerimetro();
+		perimetroCuadrado2 = cuadrado2.calcularPerimetro();
+		perimetroCuadrado3 = cuadrado3.calcularPerimetro();
+		
+		System.out.println("---AREA DEL CUADRADO----");
+
+		System.out.println("Area del cuadrado 1 : " + areaCuadrado1);
+		System.out.println("Area del cuadrado 2 : " + areaCuadrado2);
+		System.out.println("Area del cuadrado 3 : " + areaCuadrado3);
+
+		System.out.println("---PERIMETRO DEL CUADRADO----");
+
+		System.out.println("Perimetro del cuadrado 1 : " + perimetroCuadrado1);
+		System.out.println("Perimetro del cuadrado 2 : " + perimetroCuadrado2);
+		System.out.println("Perimetro del cuadrado 3 : " + perimetroCuadrado3);
+
+	}
+
+}
